99f880693b7a1ebcd0d93d8ef0192c663c77bfd7
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-21 19:24:09 +0530

    ADDED: TC1-Given message should return SAD

---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+    //Test Case 1.1
+    @Test
+    public void givenMessage_Should_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in a sad mood");
+        Assert.assertEquals("SAD", mood);
+    }
+}
+
